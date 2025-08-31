9ff3f06dcbb921c89bb79bbfdc4721d6dbc8755c
muhammadalfin9 <alfinnaufurrozaq@gmail.com>
2025-08-31 20:40:06 +0700

    mengerjakan ujian 2 dan 3

---
+++ quiz/ujian/Ujian3.js
@@ -0,0 +1,23 @@
+// Problem
+// Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu 
+// dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+// Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+
+function konversiMenit(menit) {
+    let menitNew = Math.floor(menit/60);
+    let detik = menit % 60;
+    let zeroDetik;
+    if (detik < 10){
+      zeroDetik = "0" + detik;
+    }else{
+      zeroDetik = detik;
+    }
+    return menitNew + ':' + zeroDetik;
+}
+  
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
